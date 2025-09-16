fd35ec6a6e794f2581704586e3cc9319dcfe74cf
Uryhk <uriel.rosales.decara@mi.unc.edu.ar>
2025-09-16 20:25:02 -0300

    Update Reglas.cpp

--- guia/semana6/src/Reglas.cpp
+++ guia/semana6/src/Reglas.cpp
@@ -13,5 +13,40 @@ static bool esOpBool(const std::string& t) {
 bool Reglas::evaluarRPN(Habitacion* h, Lista<std::string>* tokens) {
     // TODO
     Pila<bool> aux;
-    if(h->get_nombres != tokens
+       while (!tokens->isEmpty()) {
+        std::string token = tokens->head();
+        tokens->pop_front(); 
+
+        if (token == "AND" || token == "OR" || token == "NOT") {
+            
+            if (token == "NOT") {
+                if (aux.empty()) throw std::runtime_error("Faltan operandos");
+                bool a = aux.pop();
+                aux.push(!a);
+            } else if (token == "AND") {
+                if (aux.size() < 2) throw std::runtime_error("Faltan operandos");
+                bool b = aux.pop();
+                bool a = aux.pop();
+                aux.push(a && b);
+            } else if (token == "OR") {
+                if (aux.size() < 2) throw std::runtime_error("Faltan operandos");
+                bool b = aux.pop();
+                bool a = aux.pop();
+                aux.push(a || b);
+            }
+        } else {
+            const Sensor* s = h->obtenerConstRec(
+                h->sensores.begin(),
+                h->sensores.end(),
+                token
+            );
+            if (!s) throw std::runtime_error("Sensor no encontrado: " + token);
+
+            aux.push(s->getValor()); 
+        }
+    }
+
+    if (aux.size() != 1) throw std::runtime_error("Expresión RPN inválida");
+
+    return aux.pop(); 
 }
