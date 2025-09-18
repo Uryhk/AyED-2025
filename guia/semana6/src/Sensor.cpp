fb3f98ce1cf7cfa9aa5826c60606eb6ca442c705
Uryhk <uriel.rosales.decara@mi.unc.edu.ar>
2025-09-18 16:35:41 -0300

    Update Sensor.cpp

--- guia/semana6/src/Sensor.cpp
+++ guia/semana6/src/Sensor.cpp
@@ -10,21 +10,46 @@ Sensor::~Sensor() {
 
 void Sensor::configurarVentana(int k) {
     //TODO
- 
+    if (ventana) delete ventana;  // eliminar ventana anterior si existe
+    K = k;
+    ventana = new Cola<float>();  // crear nueva cola vacía
+    sumaVentana = 0.0f;          // reiniciar suma
+    
 }
 
 void Sensor::agregarLectura(float x) {
     // TODO
+
+    if (!ventana) return;  // si no hay ventana, no hacer nada
+    ventana->enqueue(x);
+    sumaVentana += x;
+    if (ventana->size() > K) {
+        float eliminado = ventana->dequeue();
+        sumaVentana -= eliminado;
+    }
+    valor = x;  // actualizar el valor actual del sensor
+
 }
 
 void Sensor::setValor(float nuevo) {
     //TODO
+    valor = nuevo;
+    agregarLectura(nuevo);  // alimentar la ventana si está configurada
+
 }
 
 float Sensor::promedioMovil() const {
     //TODO
+    if (!ventana || ventana->empty()) return 0.0f;  // si no hay ventana o está vacía
+    return sumaVentana / ventana->size();
+
 }
 
 bool Sensor::estable(float umbral) const {
     //TODO
+    if (!ventana || ventana->empty()) return true;  // si no hay ventana o está vacía
+    float promedio = promedioMovil();
+    return std::fabs(valor - promedio) < umbral;
+        
 }
+
